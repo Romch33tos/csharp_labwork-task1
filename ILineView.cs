2e634cdbda190f9451344076c5736d2b33bc33ff
Roman Khadraliev <Romch33tos@yandex.ru>
2025-09-07 18:37:26 +0700

    Реализован интерфейс для представления формы

---
+++ ILineView.cs
@@ -0,0 +1,10 @@
+﻿public interface ILineView
+{
+  event EventHandler CalculateRequested;
+  double CoefficientA { get; set; }
+  double PointO1X { get; set; }
+  double PointO1Y { get; set; }
+  double PointO2X { get; set; }
+  string Result { set; }
+  void ShowError(string message);
+}
